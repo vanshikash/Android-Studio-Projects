4f35e8835da0ed76795b841160eae9074538d365
Vanshika <vanshikasharma4102@gmail.com>
2021-12-27 19:01:11 +0530

    added roll button functionality

--- RolltheDice/app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ RolltheDice/app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.rollthedice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+        private fun rollDice() {
+            val dice = Dice(6)
+            val diceRoll = dice.roll()
+            val finalTextView: TextView = findViewById(R.id.textView)
+            finalTextView.text = diceRoll.toString()
+        }
+}
+
+class Dice(private val sides: Int){
+
+    fun roll() : Int{
+        return (1..sides).random()
     }
 }
