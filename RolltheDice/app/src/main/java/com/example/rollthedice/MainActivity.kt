3e1535d8b8915e471251ba1659add13803bebbd2
Vanshika <vanshikasharma4102@gmail.com>
2021-12-27 20:12:55 +0530

    added 2 dices

--- RolltheDice/app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ RolltheDice/app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+// finds roll button and uses setOnClickListener
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +17,31 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener { rollDice() }
     }
 
-        private fun rollDice() {
-            val dice = Dice(6)
-            val diceRoll = dice.roll()
-            val finalTextView: TextView = findViewById(R.id.textView)
-            finalTextView.text = diceRoll.toString()
-        }
+    //rollDice function
+    private fun rollDice() {
+        //creating dice object with six sides and rolling it
+        val dice1 = Dice(6)
+        val dice1Roll = dice1.roll()
+
+        val dice2 = Dice(20)
+        val dice2Roll = dice2.roll()
+        //To view the random output after rolling the dice
+        val finalTextView: TextView = findViewById(R.id.textView)
+        finalTextView.text = dice1Roll.toString()
+
+        val finalTextView3: TextView = findViewById(R.id.textView3)
+        finalTextView3.text = dice2Roll.toString()
+
+
+
+
+    }
 }
 
-class Dice(private val sides: Int){
+//Dice roll logic
+class Dice(private val sides: Int) {
 
-    fun roll() : Int{
+    fun roll(): Int {
         return (1..sides).random()
     }
 }
