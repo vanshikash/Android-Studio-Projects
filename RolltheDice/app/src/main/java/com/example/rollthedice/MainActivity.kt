4026171a5e8942ad82212ab6036990e0c1b70339
Vanshika <vanshikasharma4102@gmail.com>
2021-12-28 14:30:55 +0530

    updated dice image and finalised code

--- RolltheDice/app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ RolltheDice/app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.rollthedice
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +13,31 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
 
+        //Do Dice roll when button is clicked
         rollButton.setOnClickListener { rollDice() }
     }
 
         private fun rollDice() {
+            //setting number of sides of dice to 6
             val dice = Dice(6)
             val diceRoll = dice.roll()
-            val finalTextView: TextView = findViewById(R.id.textView)
-            finalTextView.text = diceRoll.toString()
+            //find imageview in layout
+            val diceImage : ImageView = findViewById(R.id.imageView)
+            //display imageview corresponding to rolled number
+            when(diceRoll)
+            {
+                1 -> diceImage.setImageResource(R.drawable.dice_1)
+                2 -> diceImage.setImageResource(R.drawable.dice_2)
+                3 -> diceImage.setImageResource(R.drawable.dice_3)
+                4 -> diceImage.setImageResource(R.drawable.dice_4)
+                5 -> diceImage.setImageResource(R.drawable.dice_5)
+                6 -> diceImage.setImageResource(R.drawable.dice_6)
+            }
+
+            //update content description
+            diceImage.contentDescription = diceRoll.toString()
+
+
         }
 }
 
